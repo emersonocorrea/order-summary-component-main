8200243e6ceb7f4c92a7318707ea536df04e50ad
Emerson Alexandre de oliveira Correa <110613926+emersonocorrea@users.noreply.github.com>
2023-11-29 13:34:42 -0300

    finish

---
+++ src/scripts/tailwind.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+    theme: {
+        fontFamily: {
+            'RedHat': ['Red Hat Display', 'sans-serif'],
+        },
+      extend: {
+        colors: {
+            paleBlue: 'hsl(225, 100%, 94%)',
+            brightBlue: 'hsl(245, 75%, 52%)',
+            veryPaleBlue: 'hsl(225, 100%, 98%)',
+            desaturatedBlue: 'hsl(224, 23%, 55%)',
+            darkBlue: 'hsl(223, 47%, 23%)',
+            backPlan: '#F7F9FF',
+        }
+      }
+    }
+  }
